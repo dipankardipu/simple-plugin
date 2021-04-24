5ea51a6a4052a0ccc62cb2215fe1f20b5ea6c16e
dipankardipu <dipankardipu@gmail.com>
2021-04-24 23:51:49 +0530

    push chnages

--- sample-plugin.php
+++ sample-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:     Sample Plugin
  * Plugin URI:      PLUGIN SITE HERE
  * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
+ * Author:          Dipankar Biswas
  * Author URI:      YOUR SITE HERE
  * Text Domain:     sample-plugin
  * Domain Path:     /languages
